19eab0f0bfe24bc1190871fbd069cd0d78ae5c63
Yana-Nechmoglod <ivashura199028@gmail.com>
2021-10-11 12:46:09 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow:'<img class="slider__arrow slider__ArrowLeft" src="images/ArrowLeft.jpg" alt="">',
+        prevArrow:'<img class="slider__arrow slider__Arrow" src="images/ArrowLeft.jpg" alt="">'
+    });
 });
